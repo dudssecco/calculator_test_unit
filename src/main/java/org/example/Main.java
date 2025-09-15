8fcaca06d2f930d786bf17f14a053c163eb93573
Secco <dudssecco.code@gmail.com>
2025-09-15 20:02:25 -0300

    Create class Claculadora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,32 @@
 package org.example;
+
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        String opcao="";
+        Calculadora calculadora = new Calculadora();
+        while(!opcao.equals("9")){
+            opcao = JOptionPane.showInputDialog("1 - Leitura \n2 - Adicona \n3 - Subtracao \n4 - Multiplicação \n5 - Divisao \n9 - Sair");
+            switch (Integer.parseInt(opcao)) {
+                case 1:
+                    calculadora.setValorA(Double.parseDouble(JOptionPane.
+                            showInputDialog("Digite o valor A")));
+                    calculadora.setValorB(Double.parseDouble(JOptionPane.
+                            showInputDialog("Digite o valor B")));
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null,"Soma: " + calculadora.getSoma());
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null,"Diferenca: " + calculadora.getDiferenca());
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,"Produto: " + calculadora.getProduto());
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null,"Quociente: " + calculadora.getQuociente());
+                    break;
+            }
+        }
     }
 }
